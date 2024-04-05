a07f652ff4c95bdc227c5b0531bb691bf8eb81a8
Jaeyoung <161218943+choicebuilds@users.noreply.github.com>
2024-04-05 10:26:15 -0400

    Replace LCD code with LED for mode changes

--- main.cpp
+++ main.cpp
@@ -7,32 +7,21 @@
 
 RawSerial  bluemod(p28,p27);
 Motor lm(p23, p6, p5); // pwm, fwd, rev
-Motor rm(p24, p8, p7); //pwm, fwd, rev
-DigitalOut led(LED1);
-Mutex mutex;
-uLCD_4DGL uLCD(p13,p14,p15); // serial tx, serial rx, reset pin;
+Motor rm(p24, p8, p7); // pwm, fwd, rev
+DigitalOut led(LED1); // on = manual; off = automatic;
 DigitalIn echo(p20);
 DigitalOut trigger(p19);
 //Analog Out Jack
 //On Board Speaker
-volatile bool mode=1; //1 = manual; 0 = automatic
-volatile int distance=0;
-volatile int correction=0;
+volatile bool mode = 1; //1 = manual; 0 = automatic
+volatile int distance = 0;
+volatile int correction = 0;
 Timer sonar;
 
 void blue(void const *args) {
-    char bnum=0;
-    char bhit=0;
+    char bnum = 0;
+    char bhit = 0;
     while(1) {
-        mutex.lock();
-        if (mode) {
-            uLCD.locate(1,2);
-            uLCD.printf("\n\nManual Mode   ");
-        } else {
-            uLCD.locate(1,2);
-            uLCD.printf("\n\nAutomatic Mode");
-        }
-        mutex.unlock();
         if (bluemod.getc()=='!') {
             if (bluemod.getc()=='B') { //button data packet
                 bnum = bluemod.getc(); //button number
@@ -41,7 +30,8 @@ void blue(void const *args) {
                     switch (bnum) {
                         case '1': //set mode
                             if (bhit=='1') {
-                                mode=!mode;
+                                mode = !mode;
+                                led = !led;
                             } else {
                                 mode = mode;
                             }
@@ -103,46 +93,32 @@ void blue(void const *args) {
 void sonarThread(void const *args) {
     sonar.reset();
     sonar.start();
-    while(echo==2) {};
+    while(echo == 2) {};
     sonar.stop();
     correction = sonar.read_us();
     while(1) {
-        trigger=1;
+        trigger = 1;
         sonar.reset();
         wait_us(10.0);
-        trigger=0;
-        while(echo==0) {};
+        trigger = 0;
+        while(echo == 0) {};
         sonar.start();
-        while(echo==1) {};
+        while(echo == 1) {};
         sonar.stop();
         distance = (sonar.read_us() - correction) / 58.0;
-        mutex.lock();
-        uLCD.locate(2,6);
-        uLCD.printf("Distance: %5.0d", distance);
-        mutex.unlock();
+//        uLCD.printf("Distance: %5.0d", distance);
         Thread::wait(200);
     }
 }
 
 int main() {
-    uLCD.cls();
-    uLCD.printf("Design Project");
+    led = 1;
     Thread thread2(blue);
     Thread thread3(sonarThread);
     while(1) {
-        led = 1;
+//        led = 1;
+//        Thread::wait(200);
+//        led = 0;
         Thread::wait(200);
-        led = 0;
-        Thread::wait(200);
-        // mutex.lock();
-        // uLCD.locate(2, 6);
-        // mutex.unlock();
-    //SD card needed with image and video files for last two demos
-        //uLCD.media_init();
-        // Thread::wait(200);
-        // uLCD.set_sector_address(0x0000, 0x0000);
-        //uLCD.display_video(0,0);
-        //Thread::wait(100);
-        //ulcd_mutex.unlock();
     }
 }
