d4776f935d6cf4135a6dfd91ab0776f9c41c0466
Shayahn Mirfendereski <shayahnmir@gmail.com>
2024-04-10 10:43:15 -0400

    Working code for manual and automatic modes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include "Mutex.h"
 #include "mbed.h"
 #include "rtos.h"
 #include "Motor.h"
 #include "Speaker.h"
 #include "SDFileSystem.h"
 #include "wave_player.h"
+#include "XNucleo53L0A1.h"
+#include <stdio.h>
 
 RawSerial  bluemod(p13, p14);
 Motor lm(p23, p6, p5); // pwm, fwd, rev
@@ -12,8 +15,14 @@ DigitalOut led(LED1); // on = manual; off = automatic;
 DigitalIn echo(p20);
 DigitalOut trigger(p19);
 Speaker mySpeaker(p21);
-//Analog Out Jack
-//On Board Speaker
+DigitalOut shdn(p26);
+Serial pc(USBTX,USBRX);
+Mutex mutex;
+
+#define VL53L0_I2C_SDA   p28
+#define VL53L0_I2C_SCL   p27
+
+static XNucleo53L0A1 *board=NULL;
 volatile bool mode = 1; //1 = manual; 0 = automatic
 volatile int distance = 0;
 volatile int correction = 0;
@@ -32,12 +41,18 @@ void blue(void const *args) {
                 if (bluemod.getc()==char(~('!' + 'B' + bnum + bhit))) { //checksum OK?
                     switch (bnum) {
                         case '1': //set mode
+                            // mutex.lock();
                             if (bhit=='1') {
                                 mode = !mode;
                                 led = !led;
+                                mutex.lock();
+                                lm.speed(0.0);
+                                rm.speed(0.0);
+                                mutex.unlock();
                             } else {
                                 mode = mode;
                             }
+                            // mutex.unlock();
                             break;
                         case '3': //decrease speed
                             if (bhit=='1') {
@@ -69,6 +84,7 @@ void blue(void const *args) {
                             break;
                         case '5': //up dpad
                             if (mode) {
+                                mutex.lock();
                                 if (bhit=='1') {
                                     lm.speed(speed);
                                     rm.speed(speed);
@@ -76,10 +92,12 @@ void blue(void const *args) {
                                     lm.speed(0.0);
                                     rm.speed(0.0);
                                 }
+                                mutex.unlock();
                             }
                             break;
                         case '6': //down dpad
                             if (mode) {
+                                mutex.lock();
                                 if (bhit=='1') {
                                     lm.speed(-speed);
                                     rm.speed(-speed);
@@ -87,10 +105,12 @@ void blue(void const *args) {
                                     lm.speed(0.0);
                                     rm.speed(0.0);
                                 }
+                                mutex.unlock();
                             }
                             break;
                         case '7': //left dpad
                             if (mode) {
+                                mutex.lock();
                                 if (bhit=='1') {
                                     lm.speed(-speed);
                                     rm.speed(speed);
@@ -98,10 +118,12 @@ void blue(void const *args) {
                                     lm.speed(0.0);
                                     rm.speed(0.0);
                                 }
+                                mutex.unlock();
                             }
                             break;
                         case '8': //right dpad
                             if (mode) {
+                                mutex.lock();
                                 if (bhit=='1') {
                                     lm.speed(speed);
                                     rm.speed(-speed);
@@ -109,6 +131,7 @@ void blue(void const *args) {
                                     lm.speed(0.0);
                                     rm.speed(0.0);
                                 }
+                                mutex.unlock();
                             }
                             break;
                         default:
@@ -121,23 +144,44 @@ void blue(void const *args) {
     }
 }
 
-void sonarThread(void const *args) {
-    sonar.reset();
-    sonar.start();
-    while(echo == 2) {};
-    sonar.stop();
-    correction = sonar.read_us();
-    while(1) {
-        trigger = 1;
-        sonar.reset();
-        wait_us(10.0);
-        trigger = 0;
-        while(echo == 0) {};
-        sonar.start();
-        while(echo == 1) {};
-        sonar.stop();
-        distance = (sonar.read_us() - correction) / 58.0;
-//        uLCD.printf("Distance: %5.0d", distance);
+void lidar(void const *args) {
+    int status;
+    uint32_t distance;
+    DevI2C *device_i2c = new DevI2C(VL53L0_I2C_SDA, VL53L0_I2C_SCL);
+    /* creates the 53L0A1 expansion board singleton obj */
+    board = XNucleo53L0A1::instance(device_i2c, A2, D8, D2);
+    shdn = 0; //must reset sensor for an mbed reset to work
+    wait(0.1);
+    shdn = 1;
+    wait(0.1);
+    /* init the 53L0A1 board with default values */
+    status = board->init_board();
+    while (status) {
+        pc.printf("Fail");
+        status = board->init_board();
+    }
+    //loop taking and printing distance
+    pc.printf("%d", mode);
+    while (1) {
+        if (!mode) {
+            pc.printf("%d", mode);
+            pc.printf("hello\n");
+            mutex.lock();
+            lm.speed(0.5);
+            rm.speed(0.5);
+            mutex.unlock();
+            status = board->sensor_centre->get_distance(&distance);
+            while (distance <= 150) {
+                pc.printf("D=%ld mm\r\n", distance);
+                //speaker make noise !!!!
+                mutex.lock();
+                lm.speed(-0.5);
+                rm.speed(0.5);
+                mutex.unlock();
+                wait(0.5);
+                status = board->sensor_centre->get_distance(&distance);
+            }
+        }
         Thread::wait(200);
     }
 }
@@ -145,7 +189,7 @@ void sonarThread(void const *args) {
 int main() {
     led = 1;
     Thread thread2(blue);
-    Thread thread3(sonarThread);
+    Thread thread3(lidar);
     while(1) {
 //        led = 1;
 //        Thread::wait(200);
