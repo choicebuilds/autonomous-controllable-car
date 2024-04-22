2a76f68aaec6f759d14b1407e9bc21557485528e
sethwobig3 <swobig3@gatech.edu>
2024-04-22 09:53:16 -0400

    made changes to lcd and lidar

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 #include "rtos.h"
 #include "Motor.h"
 #include "Speaker.h"
-#include "SDFileSystem.h"
-#include "wave_player.h"
-#include "XNucleo53L0A1.h"
+#include "uLCD_4DGL.h"
 #include <stdio.h>
+#include <VL53L0X.h>
+#include "Servo.h"
+
+
+DevI2C          i2c(p28, p27);
+DigitalOut      shdn_left(p26);
+VL53L0X         l_left(&i2c, &shdn_left, NC);
+uint32_t        distances[16];
+
+Servo myservo(p22);
 
 RawSerial  bluemod(p13, p14);
 Motor lm(p23, p6, p5); // pwm, fwd, rev
@@ -15,17 +23,13 @@ DigitalOut led(LED1); // on = manual; off = automatic;
 DigitalIn echo(p20);
 DigitalOut trigger(p19);
 Speaker mySpeaker(p21);
-DigitalOut shdn(p26);
+// DigitalOut shdn(p26);
 Serial pc(USBTX,USBRX);
+uLCD_4DGL uLCD(p9,p10,p20); // serial tx, serial rx, reset pin;
 Mutex mutex;
-Mutex speaker;
-
-#define VL53L0_I2C_SDA   p28
-#define VL53L0_I2C_SCL   p27
-
-static XNucleo53L0A1 *board=NULL;
+// Mutex speaker;
+Mutex LCD;
 volatile bool mode = 1; //1 = manual; 0 = automatic
-volatile int distance = 0;
 volatile int correction = 0;
 volatile float speed = 0.7;
 volatile double sound = 440;
@@ -64,9 +68,9 @@ void blue(void const *args) {
                                     speed -= 0.1;
                                     sound -= 30;
                                 }
-                                speaker.lock();
+                                //speaker.lock();
                                 mySpeaker.PlayNote(sound, 0.5, 1.0);
-                                speaker.unlock();
+                                //speaker.unlock();
                             } else {
                                 speed = speed;
                             }
@@ -80,9 +84,9 @@ void blue(void const *args) {
                                     speed += 0.1;
                                     sound += 30;
                                 }
-                                speaker.lock();
+                                //speaker.lock();
                                 mySpeaker.PlayNote(sound, 0.5, 1.0);
-                                speaker.unlock();
+                                //speaker.unlock();
                             } else {
                                 speed = speed;
                             }
@@ -97,7 +101,7 @@ void blue(void const *args) {
                                     lm.speed(0.0);
                                     rm.speed(0.0);
                                 }
-                                mutex.unlock();
+                               mutex.unlock();
                             }
                             break;
                         case '6': //down dpad
@@ -145,79 +149,103 @@ void blue(void const *args) {
                 }
             }
         }
-        Thread::wait(200);
+        Thread::wait(300);
     }
 }
 
 void lidar(void const *args) {
-    int status;
-    uint32_t distance;
-    DevI2C *device_i2c = new DevI2C(VL53L0_I2C_SDA, VL53L0_I2C_SCL);
-    /* creates the 53L0A1 expansion board singleton obj */
-    board = XNucleo53L0A1::instance(device_i2c, A2, D8, D2);
-    shdn = 0; //must reset sensor for an mbed reset to work
-    wait(0.1);
-    shdn = 1;
-    wait(0.1);
-    /* init the 53L0A1 board with default values */
-    status = board->init_board();
-    while (status) {
-        pc.printf("Fail");
-        status = board->init_board();
-    }
-    //loop taking and printing distance
-    pc.printf("%d", mode);
     while (1) {
+        l_left.get_distance(&distances[0]); 
+        Thread::wait(200);
+    }
+}
+void move(void const *args) {
+    while(1) {
         if (!mode) {
-            pc.printf("D=%ld mm\r\n", distance);
             mutex.lock();
             lm.speed(0.5);
             rm.speed(0.5);
             mutex.unlock();
-            status = board->sensor_centre->get_distance(&distance);
-            if ((distance > 500)) {
-                speaker.lock();
+            //speaker.lock();
+            if (((distances[0]) > 500)) {
                 mySpeaker.PlayNote(sound, 0.5, 0.1);
-                speaker.unlock();
             }
-            if ((distance < 500) && (distance >= 400)) {
-                speaker.lock();
+            else if (((distances[0]) < 500) && ((distances[0]) >= 400)) {
                 mySpeaker.PlayNote(sound, 0.5, 0.3);
-                speaker.unlock();
             }
-            if ((distance < 400) && (distance >= 300)) {
-                speaker.lock();
+            else if (((distances[0]) < 400) && ((distances[0]) >= 300)) {
                 mySpeaker.PlayNote(sound, 0.5, 0.5);
-                speaker.unlock();
             }
-            if ((distance < 300) && (distance >= 200)) {
-                speaker.lock();
+            else if (((distances[0]) < 300) && ((distances[0]) >= 200)) {
                 mySpeaker.PlayNote(sound, 0.5, 1.0);
-                speaker.unlock();
             }
-            while (distance <= 150) {
-                pc.printf("D=%ld mm\r\n", distance);
-                //speaker make noise !!!!
+            //speaker.unlock();
+            while ((distances[0]) <= 150) {
                 mutex.lock();
                 lm.speed(-0.5);
                 rm.speed(0.5);
                 mutex.unlock();
                 wait(0.5);
-                status = board->sensor_centre->get_distance(&distance);
+                // l_left.get_distance(&distances[0]);
             }
         }
-        Thread::wait(200);
+        Thread::wait(500);
+    }
+}
+void servo(void const *args) {
+    while(1) {
+        myservo = speed;
+        Thread::wait(500);
     }
 }
 
+
 int main() {
+    uLCD.cls();
+    uLCD.color(RED);
+    uLCD.filled_circle(64, 115, 10, BLUE);
     led = 1;
+
+    l_left.VL53L0X_off();
+    l_left.init_sensor(0x30);
+
     Thread thread2(blue);
     Thread thread3(lidar);
+    Thread thread4(move);
+    Thread thread5(servo);
     while(1) {
-//        led = 1;
-//        Thread::wait(200);
-//        led = 0;
-        Thread::wait(200);
+    
+        if (((distances[0]) > 500)) {
+            uLCD.filled_rectangle(32, 100, 96, 10, GREEN);
+        }
+        if (((distances[0]) >= 400)) {
+            uLCD.filled_rectangle(32, 100, 96, 30, GREEN);
+        } else {
+            uLCD.filled_rectangle(32, 10, 96, 10, BLACK);
+        }
+        if (((distances[0]) >= 300)) {
+            uLCD.filled_rectangle(32, 100, 96, 50, GREEN);
+        } else {
+            uLCD.filled_rectangle(32, 30, 96, 10, BLACK);
+        }
+        if (((distances[0]) >= 200)) {
+            uLCD.filled_rectangle(32, 100, 96, 70, GREEN);
+        } else {
+            uLCD.filled_rectangle(32, 50, 96, 10, BLACK);
+        }
+        if (((distances[0]) >= 100)) {
+            uLCD.filled_rectangle(32, 100, 96, 90, GREEN);
+        } else {
+            uLCD.filled_rectangle(32, 70, 96, 10, BLACK);
+        }
+
+        uLCD.locate(4,0);
+        if (mode) {
+            uLCD.printf("Manual   ");
+        } else {
+            uLCD.printf("Automatic");
+        }
+
+        Thread::wait(500);
     }
 }
